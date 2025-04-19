cec5663a32e9e0e972eb0256c43b86cfa13fde48
vishwanimap25 <Vishwajeet.awale@nimapinfotech.com>
2025-04-19 10:27:25 +0530

    first commit

---
+++ GenericCRUDloading/GenericCRUDloading/Generic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCRUDloading
+{
+    internal class Generic
+    {
+        public static void Examples()
+        {
+            //generic collections-> Type safe, Fixed Length
+            int[] arr = new int[3];
+            arr[0] = 1;
+            arr[1] = 2;
+            arr[2] = 3;
+            //arr[3] = 4;
+        }
+    }
+}
